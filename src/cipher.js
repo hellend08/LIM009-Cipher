37a59736bf8135a12115d474d0f4f3ff80c6d233
hellen08 <sccp.hvasquez@gmail.com>
2019-02-28 10:41:20 -0500

    confirmando archivos

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,45 @@
 window.cipher = {
-  encode: () => {
+  
+  encode: (numberOffset, readTex,texTwo2) => {
     /* Acá va tu código */
+    console.log(numberOffset.value);
+      let newNumber;  
+      let ascii;
+      let newTex = " ";
+      let textoDescifrado = readTex;
+
+    for(let i=0; i<textoDescifrado.length; i++) {
+       ascii=textoDescifrado.charCodeAt(i);
+       if( 65 <= ascii <= 90){
+       newNumber=String.fromCharCode((ascii - 65 + numberOffset.value) % 26 + 65);
+       console.log(newNumber);
+       var sum = newTex += newNumber;/*String.fromCharCode*/ 
+       console.log(sum);
+       texTwo2.innerHTML = sum ;
+      }
+    }
+
   },
-  decode: () => {
-    /* Acá va tu código */
+
+
+  /* Acá va tu código */
+  decode: (numberOffset, readTex,texTwo2) => {
+    let newNumber;  
+      let ascii;
+      let newTex = " ";
+      let textoDescifrado = readTex;
+
+    for(let i=0; i<textoDescifrado.length; i++) {
+       ascii=textoDescifrado.charCodeAt(i);
+       if( 65 <= ascii <= 90){
+       newNumber=String.fromCharCode(( parseInt(ascii) - 65 - parseInt(numberOffset.value)) % 26 + 65 +  parseInt(numberOffset.value));
+       console.log(newNumber);
+       var sum = newTex += newNumber;/*String.fromCharCode*/ 
+       console.log(sum);
+       texTwo2.innerHTML = sum ;
+      }
+    }
+
+   
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,103 @@
 
 /* Acá va tu código */
+let pagOne = document.getElementById("root");
+let button1 = document.getElementById("button");
+let pagTwo = document.getElementById("pagTwo")
+let getBack1 = document.getElementById("getBack")
+
+pagTwo.style.display = "none";
+
+button1.addEventListener("click", function(){
+    pagOne.style.display = "none";
+    button1.style.display = "block";
+    pagTwo.style.display = "block";
+})
+
+getBack1.addEventListener("click", function(){
+    pagOne.style.display = "block";
+    pagTwo.style.display = "none";
+    
+})
+// obteniendo valor del input numero para los espacios:
+
+
+const texOne1 = document.getElementById("texOne");
+const buttonCipher1 = document.getElementById("buttonCipher");
+const buttonCipher2 = document.getElementById("buttonDecipher");
+const texTwo2 = document.getElementById("texTwo")
+
+const numberOffset = document.getElementById("offset");
+
+console.log(numberOffset.value);
+
+
+var encode = window.cipher;
+
+buttonCipher1.addEventListener("click", function (){
+    
+    const readTex = (texOne1.value).toUpperCase();
+    const answerTex = texTwo2.value;
+     
+    /*console.log*/(readTex);
+    cipher.encode(numberOffset, readTex, texTwo2)
+   
+
+})
+
+buttonCipher2.addEventListener("click", function (){
+    
+    const readTex = (texOne1.value).toUpperCase();
+    const answerTex = texTwo2.value;
+     
+    console.log(readTex);
+    cipher.decode(numberOffset, readTex, texTwo2)
+   
+
+})
+
+
+
+/*document.getElementById("buttonCipher").addEventListener("click",() =>{
+    let textoDescifrado = document.getElementById("texOne").value;
+    let desplazamiento = document.getElementById("offset").value;
+    newTex=cipher.encode(textoDescifrado, desplazamiento);
+    document.getElementById("texTwo").value = newTex;
+
+
+})*/
+
+/*
+let oneTex= "diana";
+let offset= 3;
+let ascii;
+let numberOff;
+let texTwo;
+
+for(let i = 0; i < oneTex.length; i++){
+    ascii=oneTex[i].charCodeAt();
+    numberOff= ascii + offset;
+    console.log(texTwo=String.fromCharCode(numberOff));
+}
+*/
+ 
+/*
+const letra = 'B';
+const numero = 4;
+const assci = letra.charCodeAt();
+console.log(assci);
+const nuevoNumero =  assci + numero;
+console.log(nuevoNumero)
+
+console.log(String.fromCharCode(nuevoNumero));
+
+let palabra="ABC";
+let desplazamiento= 3;
+let ascii;
+let nuevoNumero;
+let nuevaPalabra;
+
+for(let i=0; i< palabra.length;i++){
+  ascii=palabra[i].charCodeAt();
+  nuevoNumero=ascii + desplazamiento;
+  console.log(nuevapalabra=String.fromCharCode(nuevoNumero));
+  */
