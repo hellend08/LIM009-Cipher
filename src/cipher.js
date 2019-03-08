c6742f1e102ad4b185aa886d13d1f23834a40c54
hellen08 <sccp.hvasquez@gmail.com>
2019-03-08 08:52:14 -0500

    actualizando cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,33 +1,22 @@
 window.cipher = {
-  
   encode: (mayusText, offsetFinal) => {
-   
     let cipherText;  
     let ascii;
-    //let count = " ";
     let concatCipherText = "";
-    
-
-    for(let i = 0; i < mayusText.length; i++) {
+    for ( let i = 0; i < mayusText.length; i++) {
       ascii = mayusText.charCodeAt(i);
       cipherText = String.fromCharCode((parseInt(ascii) - 65 + parseInt(offsetFinal)) % 26 + 65);
-        //concatCipherText = 
-      concatCipherText += cipherText;     
-    
+      concatCipherText += cipherText;  
     } 
     return concatCipherText;
   },
-  
   decode: (mayusText, offsetFinal) => {
-     
     let decipherText;  
     let ascii;
-    let concatDecipherText = " ";
-
+    let concatDecipherText = "";
     for(let i = 0; i < mayusText.length; i++) {
       ascii = mayusText.charCodeAt(i);
       decipherText = String.fromCharCode((parseInt(ascii) + 65 - parseInt(offsetFinal)) % 26 + 65);     
-        //const concatDecipherText = 
       concatDecipherText += decipherText;
     }
     return concatDecipherText;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,34 @@
-let pagOne = document.getElementById("pagOne");
-let buttonLink = document.getElementById("buttonLink");
-let pagTwo = document.getElementById("pagTwo");
-let buttonGetBack = document.getElementById("buttonGetBack");
+const pagOne = document.getElementById("presentation");
+const buttonLink = document.getElementById("button-link");
+const pagTwo = document.getElementById("encode-decode");
+const buttonGetBack = document.getElementById("button-get-back");
+const numberOffset = document.getElementById("number-offset");
+const inputText = document.getElementById("input-text");
+const buttonCipher = document.getElementById("button-cipher");
+const buttonDecipher = document.getElementById("button-decipher");
+const boxAnswerText = document.getElementById("box-answer-text");
 
 pagTwo.style.display = "none";
 
 buttonLink.addEventListener("click", function(){
-
     pagOne.style.display = "none";
     pagTwo.style.display = "block";
 });
 
 buttonGetBack.addEventListener("click", function(){
     pagOne.style.display = "block";
-    pagTwo.style.display = "none";
-    
-})
-// obteniendo valor del input numero para los espacios:
-const numberOffset = document.getElementById("numberOffset");
-const inputText = document.getElementById("inputText");
-const buttonCipher = document.getElementById("buttonCipher");
-const buttonDecipher = document.getElementById("buttonDecipher");
-const boxAnswerText = document.getElementById("boxAnswerText");
-
-buttonCipher.addEventListener("click", function (){
-    let mayusText = (inputText.value).toUpperCase(); 
-    let offsetFinal = numberOffset.value;
-
-    let answerCipher = cipher.encode(mayusText, offsetFinal);
-
-
-    boxAnswerText.innerHTML = answerCipher;
-
+    pagTwo.style.display = "none";   
 });
 
-buttonDecipher.addEventListener("click", function (){
-    let mayusText = (inputText.value).toUpperCase(); 
-    let offsetFinal = numberOffset.value;
+buttonCipher.addEventListener("click", function(){
+    const mayusText = (inputText.value).toUpperCase(); 
+    const offsetFinal = numberOffset.value;
+    boxAnswerText.innerHTML = cipher.encode(mayusText, offsetFinal);
+});
 
-   
-    let answerDecipher = cipher.decode(mayusText, offsetFinal);
-    boxAnswerText.innerHTML = answerDecipher;
-
-})
+buttonDecipher.addEventListener("click", function(){
+    const mayusText = (inputText.value).toUpperCase(); 
+    const offsetFinal = numberOffset.value;
+    boxAnswerText.innerHTML = cipher.decode(mayusText, offsetFinal);
+});
 
